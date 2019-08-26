4b9e2c91daa5cfa310944d168f6d539b91eb7dd3
cowankc <cowankc92@live.com>
2019-08-26 12:35:45 -0500

    created functions to display buttons and retrieve data from api to be displayed on screen

---
+++ docs/assets/javascript/logic.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+// api key = 7Kgo6f53wQMEg787rQVK2kns5fflfzYB
+
+let teams = ["San Antonio Spurs", "Baltimore Ravens", "Portland Trailblazers"];
+
+function displayGif () {
+    event.preventDefault();
+    console.log($(this).attr("data-name"))
+    const url = "https://api.giphy.com/v1/gifs/search?q=" + $(this).attr("data-name") + "&api_key=7Kgo6f53wQMEg787rQVK2kns5fflfzYB&limit=10"
+     $.ajax({
+        url,
+        method:"GET"
+    })
+    .then (function(response) {
+        let gifs = response.data;
+        for (let i = 0; i < gifs.length; i++) {
+        let newDiv = $("<div>");
+        let rating = gifs[i].rating;
+        let p = $("<p>").text("Rating: " + rating);
+        let imgTag = $("<img>");
+        imgTag.attr("src", gifs[i].images.fixed_height.url);
+        newDiv.prepend(p);
+        newDiv.prepend(imgTag)
+        $("#results").prepend(newDiv)
+        }
+    })
+}
+
+let generateButtons = function () {
+    $("#buttons").empty();
+    for (var i = 0; i < teams.length; i++) {
+      var a = $("<button>");
+      a.addClass("team");
+      a.attr("data-name", teams[i]);
+      a.text(teams[i]);
+      $("#buttons").append(a);
+    }
+  }
+$(document).on("click", ".team", displayGif);
+generateButtons()
+})
